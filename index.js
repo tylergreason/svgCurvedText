ff8e0c298c7ea0f3f8c0cc705ab4d775acc2d566
tylergreason <tyler.greason@protonmail.com>
2020-07-30 16:48:54 -0400

    Cant seem to draw a path using document fragments that append to the SVG element. Will try again tomorrow:

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const makeArc = (x,y,rx,ry) =>{
     return document.createRange().createContextualFragment(`
     <path 
     class='arcs'
-        d="
-          M ${x}, ${y}
-          a ${rx},${ry} 0 1,1 50,0
-          a ${rx},${ry} 0 1,1 -50,0
-          " 
-          fill="red"
+    d="
+    M 100, 100
+          a 25,25 0 1,1 50,0
+          a 25,25 0 1,1 -50,0
+    " 
           />
+          fill="blue"
     
     `)
 }
@@ -16,7 +16,7 @@ const makeArc = (x,y,rx,ry) =>{
 let svg = Array.from(document.getElementsByTagName('svg'))[0]
 console.log(svg)
 for (let i = 0; i < 10; i++){
-    let newArc = makeArc(50,50,50+(10*i),50+(10*i));
+    let newArc = makeArc(0,0,5+(10*i),5+(10*i));
     svg.appendChild(newArc)
 }
 console.log(document.getElementsByClassName('arcs'));
