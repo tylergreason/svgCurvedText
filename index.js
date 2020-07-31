d18727e6a323ca2d40db63e129a2623f4164f122
tylergreason <tyler.greason@protonmail.com>
2020-07-31 09:25:06 -0400

    Finally got all the text showing! Now trying to align it vertically

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const makeArc = (x,y,rx,ry) =>{
     <path 
     class='arcs'
     d="
-    M 100, 100
+    M 10, 10
           a 25,25 0 1,1 50,0
-          a 25,25 0 1,1 -50,0
-    " 
+          a 25,25 0 1,1 -50,0" 
+    fill="blue"
           />
-          fill="blue"
     
     `)
 }
@@ -16,7 +15,29 @@ const makeArc = (x,y,rx,ry) =>{
 let svg = Array.from(document.getElementsByTagName('svg'))[0]
 console.log(svg)
 for (let i = 0; i < 10; i++){
-    let newArc = makeArc(0,0,5+(10*i),5+(10*i));
-    svg.appendChild(newArc)
+    // let newArc = makeArc(0,0,5+(10*i),5+(10*i));
+    // svg.appendChild(newArc)
+
+    newpath = document.createElementNS('http://www.w3.org/2000/svg',"path");  
+    let color = `rgba(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+    let increment = i*10; 
+    let size = 100 - increment
+    newpath.setAttribute("d",`M ${150+increment}, 150 a ${size}, ${size} 0 1,1 ${size*2},0 a ${size}, ${size} 0 1,1 ${size*-2},0`)
+    newpath.setAttribute('fill', color)
+    newpath.setAttribute('side', "right")
+    newpath.classList.add('newpath')
+    newpath.setAttribute('id', `newpath${i}`)
+    // newpath.id = `newpath${i}`
+
+    let text = document.createElementNS('http://www.w3.org/2000/svg',"text")
+    text.setAttribute('y', `${150}`)
+    text.setAttribute('x', `${150}`)
+    let newtext = document.createElementNS('http://www.w3.org/2000/svg',`textPath`)
+    newtext.textContent = `newpath${i}`
+    newtext.setAttribute("href", `#newpath${i}`)
+
+    text.appendChild(newtext)
+    svg.appendChild(text)
+    svg.appendChild(newpath)
 }
 console.log(document.getElementsByClassName('arcs'));
